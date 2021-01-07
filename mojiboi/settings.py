d772a088a6a902a56e246da1fdcec6b13a22ab61
hunterbear97 <hlytle@student.neumont.edu>
2021-01-07 14:13:57 -0700

    Changed part of the code

--- mojiboi/settings.py
+++ mojiboi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'i-cj(&x$lr1)+d1onfp3+*n(9(5(rn
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'www.mojiboi.com', 'mojiboi.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.mojiboi.com']
 
 
 # Application definition
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
